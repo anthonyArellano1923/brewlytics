89766f84a70de119b9f06d2bcbcf25809b2f9d52
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-06-28 19:16:23 -0400

    Actualizar lógica de inserción en insert_beers.py para incluir la ruta correcta del archivo JSON y ajustar la consulta SQL.

--- src/etl/insert_beers.py
+++ src/etl/insert_beers.py
@@ -1,6 +1,20 @@
 import pathlib, json, mysql.connector as mc
 
-RAW_FILE = pathlib.Path(__file__).parents[2] / "data" / "raw"
+RAW_FILE = pathlib.Path(__file__).parents[2] / "data" / "raw" / "beers.json"
+
+INSERT_SQL = """
+  INSERT INTO Beers 
+    (api_id, name, description, abv, ibu, ebc, volume_oz)
+  Values (%(id)s, %(name)s, %(description)s, %(abv)s, %(ibu)s, %(ebc)s, %(volume_oz)s)
+  ON DUPLICATE KEY UPDATE
+    name = VALUES(name), 
+    description = VALUES(description), 
+    abv = VALUES(abv), 
+    ibu = VALUES(ibu), 
+    ebc = VALUES(ebc), 
+    volume_oz = VALUES(volume_oz),
+    updated_at = NOW()
+"""
 
 def getConnection():
     return mc.connect(
@@ -11,19 +25,28 @@ def getConnection():
         autocommit=True
     )
 
-INSERT_SQL = """
-  INSERT INTO Beers 
-    (api_id, name, description, brewery_id, 
-    image_url, abv, ibu, ebc, volume_oz)
-  Values (%(id)s, %(name)s, %(description)s, %(brewery_id)s, 
-          null, %(abv)s, %(ibu)s, %(ebc)s, %(volume_oz)s)
-  ON DUPLICATE KEY UPDATE
-    name = VALUES(name), 
-    description = VALUES(description), 
-    image_url = null, 
-    abv = VALUES(abv), 
-    ibu = VALUES(ibu), 
-    ebc = VALUES(ebc), 
-    volume_oz = VALUES(volume_oz),
-    updated_at = NOW()
-"""
+def trimSpecs(row): #This function returns a dict that contains only the values required by the DB
+    requiredValues = ['id', 'name', 'description', 
+     'abv', 'ibu', 'ebc', 'volume'] 
+    newRow = {}
+    for attribute in requiredValues:
+        if attribute == 'volume':
+          newRow['volume_oz']= round(int(row[attribute]['value']) * 0.033814)
+          continue
+        newRow[attribute] = row[attribute]
+    return newRow
+
+def main():
+    rows = json.loads(RAW_FILE.read_text())
+    connection = getConnection()
+    cursor = connection.cursor()
+
+    for singleRow in rows:
+        row = trimSpecs(singleRow)
+        cursor.execute(INSERT_SQL, row)
+        print(f"Insertada.")
+
+    print(f"{cursor.rowcount} filas insertadas/actualizadas")
+
+if __name__ == '__main__':
+    main()
